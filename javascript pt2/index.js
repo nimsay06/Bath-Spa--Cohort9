328dae97bd81edccdc24821a882dc6ed17ae09b4
nimsay06 <ytercenio@yahoo.co.uk>
2022-09-27 16:44:12 +0100

    added comments to code

--- javascript pt2/index.js
+++ javascript pt2/index.js
@@ -1,9 +1,13 @@
+//Calculation function
+
 function equalsTo() {
+//setting up variables for 2 numbers
   const x = parseInt(document.getElementById("firstNumber").value);
   const y = parseInt(document.getElementById("secondNumber").value);
+  //const for whether user uses +-x÷
   const operator = document.getElementById("operator").value;
   var result;
-
+//if else statements to go through different operators
   if (operator === "add") {
     result = x + y;
   } else if (operator === "subtract") {
@@ -13,5 +17,6 @@ function equalsTo() {
   } else if (operator === "divide" && y != 0) {
     result = x / y;
   }
+  //displays result of equation to html
   document.getElementById("answer").innerHTML = result;
 }
--- javascript pt2/shopping list task/shopping.js
+++ javascript pt2/shopping list task/shopping.js
@@ -1,24 +1,10 @@
-// document.querySelector("#listInput").addEventListener("keydown", (event) => {
-//   if (event.key == "Enter") addItems();
-// });
-
-// function addItems() {
-//   var items = [];
-//   let itemValue = document.getElementById("listInput").value;
-//   items.push(itemValue);
-//   const newItem = document.createElement("li");
-//   newItem.textContent = `${addedItem}`;
-//   console.log(items);
-//   document.querySelector("#listItems").appendChild(addedItem);
-// }
-// ///add items to list, display items, alert item already added if in lis//
-// //push input of input field to items
+//create item array
 var items = [];
-
+//event listener if enter button was clicked which starts function
 document.querySelector("#listInput").addEventListener("keydown", (event) => {
   if (event.key == "Enter") addItem();
 });
-
+//function to add items to list/check if item already there/if item name is valid
 addItem = () => {
   let itemValue = document.getElementById("listInput").value;
   if (items.includes(itemValue)) {
@@ -27,7 +13,7 @@ addItem = () => {
     alert("Enter valid item name");
   } else {
     items.push(itemValue);
-    const newItem = document.createElement("h2");
+    const newItem = document.createElement("h3");
     newItem.textContent = "- " + document.querySelector("#listInput").value;
     document.querySelector("#listItems").appendChild(newItem);
     document.querySelector("#listInput").value = "";
