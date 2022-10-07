45445cbf623bd852349ae2aff8be3310416445b0
nimsay06 <ytercenio@yahoo.co.uk>
2022-10-07 16:25:59 +0100

    added function for color change
    -also finalised css and html

--- form-challenge/interactive-rating-component-main/index.js
+++ form-challenge/interactive-rating-component-main/index.js
@@ -3,7 +3,9 @@ const form = document.querySelector(".rate_form");
 const thankMessage = document.querySelector("#submittedRating");
 const submitted = document.querySelector(".submitted");
 const mainText = document.querySelector(".main");
+const ratingBtns = document.querySelectorAll(".formLabel");
 
+// Function for form submission including text change once submitted
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const rating = e.target["rating"].value;
@@ -13,3 +15,10 @@ form.addEventListener("submit", (e) => {
   mainText.classList.add("remove");
 });
 
+// Function to change background color of button once pressed
+ratingBtns.forEach((ratingBtn) => {
+  ratingBtn.addEventListener("click", () => {
+    ratingBtns.forEach((ratingBtn) => ratingBtn.classList.remove("selected"));
+    ratingBtn.classList.add("selected");
+  });
+});
